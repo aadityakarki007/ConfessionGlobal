079d3dad98ef38dda7a571b6cb658e97e651e4c1
Aaditya Karki <152679463+aadityakarki007@users.noreply.github.com>
2025-09-18 11:31:40 +0545

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,15 @@
-import { authMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
 
-export default authMiddleware()
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in/*"], // these routes are open
+  afterAuth(auth, req) {
+    // Only protect admin routes
+    if (req.nextUrl.pathname.startsWith("/admin")) {
+      if (!auth.user || auth.user.publicMetadata.role !== "confess") {
+        return redirectToSignIn(); // not allowed → redirect
+      }
+    }
+  },
+});
 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+export const config = { matcher: ["/admin/:path*"] }; // only applies to admin paths
